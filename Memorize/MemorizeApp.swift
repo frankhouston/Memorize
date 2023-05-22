2f81f64c8b54bfeecef81d5aed0110be4ea4806b
Frank Houston <frank_houston@icloud.com>
2023-05-22 07:33:32 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Frank Houston on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
